a3956e151b672b867701c1435c34b7a46a3ec88d
Sebastian <sebastian.paternostro@gmail.com>
2023-08-08 17:59:49 -0300

    commit 1

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
+import Header from './components/header/Header';
 
 function App() {
   return (
     <div className="App">
+      <Header /> 
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Hello, World.
         </p>
         <a
           className="App-link"
