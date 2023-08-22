71c7168a77d0c09ebb063772fca947acceba688a
Sebastian <sebastian.paternostro@gmail.com>
2023-08-21 21:39:44 -0300

    -

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import { useState } from 'react';
 import { v4 as uuidv4 } from 'uuid';
 import './App.css';
-import Header from './components/header/header.jsx';
+import Header from './components/header/Header.jsx';
 import Formulario from './components/form/Formulario.jsx';
 import MiOrg from './components/MiOrg';
 import Equipo from './components/Equipo';
